3aa49bd141ec0b50e80dff1a70011a9952ded714
Shaun McDonald <borgrancher@icloud.com>
2022-11-06 04:16:07 +0200

    :arrow_up: (app): upgraded dependencies

--- app/src/main/java/com/example/busschedule/database/AppDatabase.kt
+++ app/src/main/java/com/example/busschedule/database/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import com.example.busschedule.database.schedule.Schedule
 import com.example.busschedule.database.schedule.ScheduleDao
 
-@Database(entities = arrayOf(Schedule::class), version = 1, exportSchema = false)
+@Database(entities = [Schedule::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun scheduleDao(): ScheduleDao
 
